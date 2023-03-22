040395d4bc9b89fa7dc1284a8c9b18350c129631
morgennea <128577349+morgennea@users.noreply.github.com>
2023-03-22 03:27:59 -0400

    Add files via upload

---
+++ lab6_cop3502.py
@@ -0,0 +1,36 @@
+#morgenne
+def decode(encoded_password):
+    #will implement functionality later
+    decoded = ""
+    for i in encoded_password:
+        decoded += str(((int(i) - 3) + 10) % 10)
+    return decoded
+
+def encode(original_password):
+    encoded = ""
+    for i in original_password:
+        encoded += str((int(i)+3)%10)
+    return encoded
+
+loop=True
+while loop== True:
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    option = int(input("Please enter an option: "))
+
+    if option == 1:
+        password = input("Please enter your password to encode: ")
+        code = password
+        print("Your password has been encoded and stored!")
+        new_password = encode(code)
+
+    elif option == 2:
+        decoded_password = decode(new_password)
+        print(f"The encoded password is {new_password}, and the original password is {decoded_password}.")
+
+    elif option == 3:
+        loop = False
+        break
